0b68a52b1d15f03c8ef91a0d386cf7b8e458695d
Spax19 <103131298+Spax19@users.noreply.github.com>
2025-08-05 00:11:27 +0200

    ....

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,10 +1,9 @@
-// Import the necessary Firebase functions
-import { initializeApp, getApps, getApp } from "firebase/app";
+// firebase/config.js
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore"; // If you're using Firestore
 
-// Your Firebase configuration using environment variables
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,14 +11,13 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
+// Initialize Firebase services
 const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export default app;
+const db = getFirestore(app);
+
+export { auth, db };
